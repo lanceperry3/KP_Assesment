e4b491499ab15172811a86f78f8df7b33d24c81b
lanceperry3 <46603782+lanceperry3@users.noreply.github.com>
2023-01-09 09:36:22 -0800

    Create Question_3.sql

---
+++ Question_3.sql
@@ -0,0 +1,21 @@
+--Question 3:	
+--Write a query that return the phone number of the third youngest person(s) from the PRSN table (use analytic function if possible)	
+
+SELECT 
+		ao."NAME"
+        ,CASE WHEN phn."USG_TYP" 	IS NULL THEN '(null)' ELSE phn."USG_TYP" 	END AS USG_TYP
+        ,CASE WHEN phn."PHONE_NBR" 	IS NULL THEN '(null)' ELSE phn."PHONE_NBR" 	END AS PHONE_NBR
+FROM        
+	(
+      SELECT
+
+          DENSE_RANK () OVER (ORDER BY p."AGE" DESC) AS rn
+          ,p."AGE"
+          ,p."PRSN_IK"
+          ,p."NAME"
+      FROM 
+      "PRSN"					P
+    ) ao
+     LEFT JOIN   "PRSN_PHN"	phn	ON ao."PRSN_IK" = phn."PRSN_IK"
+WHERE	
+		ao.rn =3
