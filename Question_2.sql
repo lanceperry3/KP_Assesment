b00b3fabba2f67637bb95c5b8e9b5e3ae78f85f6
lanceperry3 <46603782+lanceperry3@users.noreply.github.com>
2023-01-09 10:08:59 -0800

    Update Question_2.sql

--- Question_2.sql
+++ Question_2.sql
@@ -1,11 +1,11 @@
 SELECT 
-		    ao."NAME"
+	ao."NAME"
         ,addr."ADDR"
 FROM        
 	(
       SELECT
 
-          ROW_NUMBER () OVER (ORDER BY p."AGE" DESC) AS rn
+          ROW_NUMBER () OVER (ORDER BY p."AGE" DESC) AS rn -- Add a row number descending by age leaving the oldest as row 1
           ,p."AGE"
           ,p."PRSN_IK"
           ,p."NAME"
@@ -14,4 +14,4 @@ FROM
     ) ao
      LEFT JOIN   "PRSN_ADDR"	ADDR	ON ao."PRSN_IK" = addr."PRSN_IK"
 WHERE	
-		ao.rn =1
+		ao.rn =1 -- pull only the first row which id the oldest person
